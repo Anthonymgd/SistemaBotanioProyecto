353da1aa5eeaa66ad20188d0875ae98c8bb0ab90
Amgoyes <amgoyes@espe.edu.ec>
2022-06-01 19:58:14 -0500

    Creación de un Componente Shadow DOM de un loading para la página web

---
+++ componenteShadowDOM.js
@@ -0,0 +1,101 @@
+class Figura extends HTMLElement{
+    constructor(){
+        super();
+    }
+    connectedCallback(){
+        let shadowRoot = this.attachShadow({mode:'open'})
+        shadowRoot.innerHTML = `
+        <style>
+        .container {
+            top: 50%;
+            position: relative;
+            width: 100%;
+            display: flex;
+            justify-content: center;
+        }
+        .wrapper {
+            position: absolute;
+            top: -35px;
+            transform: scale(1.5);
+        }
+        .loader {
+            height: 25px;
+            width: 1px;
+            position: absolute;
+            animation: rotate 3.5s linear infinite;
+        }
+        .loader .dot {
+            height: 7px;
+            width: 7px;
+            background: #fff;
+            border-radius: 50%;
+            position: relative;
+            background-color: black;
+            top: 40px;
+        }        @keyframes rotate {
+            30% {
+                transform: rotate(220deg);
+            }
+            40% {
+                transform: rotate(450deg);
+                opacity: 1;
+            }
+            75% {
+                transform: rotate(720deg);
+                opacity: 1;
+            }
+            76% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 0;
+                transform: rotate(0deg);
+            }
+        }
+ 
+        .loader:nth-child(1) {
+            animation-delay: 0.15s;
+        }
+ 
+        .loader:nth-child(2) {
+            animation-delay: 0.3s;
+        }
+        .loader:nth-child(3) {
+            animation-delay: 0.45s;
+        }
+        .loader:nth-child(4) {
+            animation-delay: 0.6s;
+        }
+        .loader:nth-child(5) {
+            animation-delay: 0.75s;
+        }
+        .loader:nth-child(6) {
+            animation-delay: 0.9s;
+        }
+    </style>
+    <div class="container">
+        <div class="wrapper">
+            <div class="loader">
+                <div class="dot"></div>
+            </div>
+            <div class="loader">
+                <div class="dot"></div>
+            </div>
+            <div class="loader">
+                <div class="dot"></div>
+            </div>
+            <div class="loader">
+                <div class="dot"></div>
+            </div>
+            <div class="loader">
+                <div class="dot"></div>
+            </div>
+            <div class="loader">
+                <div class="dot"></div>
+            </div>
+        </div>
+    </div>`;
+        }
+ 
+}
+window.customElements.define('carga-animacion', Figura);
