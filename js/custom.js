0c8b8e77e0fe2e54750b96e7393caefc51f365d8
Celucio <75482093+Celucio@users.noreply.github.com>
2022-06-05 15:00:28 -0500

    Custom Creado

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,36 @@
-class Nose extends HTMLElement {
+class Texto extends HTMLElement {
     constructor() {
         super();
     }
 
     connectedCallback() {
-        this.innerHTML = ``
+        this.innerHTML = `
+        <style>
+            h1{
+                font-weight: 400px;
+                text-align: center;
+                font-size: 120px;
+                line-height: 160px;
+                margin-left: -10px;
+                color: transparent;
+                -webkit-text-stroke: 1px #fff;
+                background: url(../images/back.png);
+                -webkit-background-clip: text;
+                animation: back 20s linear infinite;
+            }
+            @keyframes back {
+                100% {
+                    background-position: 2000px 0;
+                }
+            }
+        </style>
+
+      
+        <h1>Botánica</h1>
+       
+        
+        `
     }
 
 }
-window.CustomElement.define('nose', Nose);
+window.customElements.define('texto-animado', Texto);
