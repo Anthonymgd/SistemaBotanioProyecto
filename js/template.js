217eb370bb02589d135b996db63a7778d54ec5eb
JoanC07 <75482342+JoanC07@users.noreply.github.com>
2022-06-05 16:22:43 -0500

    Plantilla con HTML Template

--- js/template.js
+++ js/template.js
@@ -1,13 +1,13 @@
-class SellLabel  extends HTMLElement {
+class Estrella  extends HTMLElement {
     //Aqui iria el codigo del elemento
     constructor() {
       super();
     }
     connectedCallback(){//Nuevo elememto
       let shadowRoot=this.attachShadow({mode:'open'});  //Para establecer el ShadowDom
-      const t = document.querySelector('#final');
+      const t = document.querySelector('#Estrella');
       const instancia = t.content.cloneNode(true);
       shadowRoot.appendChild(instancia); //Nodo raiz le añade un hijo (template añade a la instancia)
      }
     }
-  window.customElements.define('boton-venta', SellLabel );
+  window.customElements.define('boton-venta', Estrella );
