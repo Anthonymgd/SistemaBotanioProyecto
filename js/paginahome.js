8d4277286cbd7afd4badf3188164eb1687fd9bcc
Amgoyes <amgoyes@espe.edu.ec>
2022-06-07 10:12:38 -0500

    a

--- js/paginahome.js
+++ js/paginahome.js
@@ -1,4 +1,4 @@
 export function cambia_de_pagina(){
-    alert("Haz ingresado con exito, ahora te redireccionaremos a la pagina principal");
-    window.location.href = "./home.html";
+    alert("Haz ingresado con exito, ahora te redireccionaremos a la pagina principal")
+    document.location.href="home.html"
 }
