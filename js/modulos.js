4ffa6545ae44d5db6123085413b63dfc85e96602
LuisOlalla <75482079+LuisOlalla@users.noreply.github.com>
2022-06-07 00:40:43 -0500

    cambios

--- js/modulos.js
+++ js/modulos.js
@@ -1,19 +1,20 @@
-import { registrar } from "./codigo.js";
+//Importar dos funciones
+import { registrar, crearTabla } from "./codigo.js";
 
-import { crearTabla } from "./codigo.js";
-
+//importar una funcion
 import { mostrarModal} from "./codigo.js";
-
-import { cerrarModal} from "./codigo.js";
+//Poner un alias
+import { cerrarModal as cierremodal} from "./codigo.js";
 // registrar();
 crearTabla();
 // mostrarModal();
 // cerrarModal();
-
+  //Mostrar el modal
   let mostrarModal1=document.getElementById("mostrar1")
   mostrarModal1.addEventListener("click",mostrarModal)
-  
+  //Registrarse
   let registro=document.getElementById("enviar1")
   registro.addEventListener("click",registrar)
+  //Cerrar el modal
    let cierre=document.getElementById("cerrar")
-   cierre.addEventListener("click",cerrarModal)
+   cierre.addEventListener("click",cierremodal)
