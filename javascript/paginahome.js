5293c274991ae3533166a85949c61f21c887689c
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-06-07 09:20:33 -0500

    modificacion

--- javascript/paginahome.js
+++ javascript/paginahome.js
@@ -1,4 +1,4 @@
 export function cambia_de_pagina(){
     alert("Haz ingresado con exito, ahora te redireccionaremos a la pagina principal")
-    location.href="http://home.html"
+    location.href="http://127.0.0.1:5501/home.html"
 }
