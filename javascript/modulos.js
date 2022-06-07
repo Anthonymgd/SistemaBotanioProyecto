d40471b28c89a5403b872a8362c36739482b3489
Selena26 <75510255+Selena26@users.noreply.github.com>
2022-06-07 09:30:34 -0500

    verificaciones

--- javascript/modulos.js
+++ javascript/modulos.js
@@ -3,11 +3,11 @@ document.adoptedStyleSheets = [sheet];
 
 import {validarEmail} from './validaremail.js';
 let email= document.getElementById("usuario")
-email.addEventListener("click",validarEmail)
+email.addEventListener("onclick",validarEmail)
 
 import {cambia_de_pagina} from './paginahome.js';
 let cambio= document.getElementById("boton")
-cambio.addEventListener("click",cambia_de_pagina)
+cambio.addEventListener("onclick",cambia_de_pagina)
 
 import {validar_formulario} from './formulario.js';
 let validar= document.getElementById("boton")
